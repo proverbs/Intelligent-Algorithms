96bc8268de5de88aa1fdc38bb37a7e89d620fe5f
proverbs <379548839@qq.com>
2015-09-03 17:17:05 +0800

    add many tsp codes

--- JavaTest/src/packageTest/ClassTest.java
+++ JavaTest/src/packageTest/ClassTest.java
@@ -1,7 +1,31 @@
 package packageTest;
 
+import java.math.BigInteger;
+import java.util.*;
+
 public class ClassTest {
 	public static void main(String[] args) {
-		System.out.println("Hello Java");
+		Ep a = new Ep(10);
+		Ep b = new Ep(9);
+		//swap(a, b);
+		Ep aa = a;
+		Ep bb = b;
+		Ep c = aa;
+		aa = bb;
+		bb = c;
+		System.out.println(b.x);
 	}
+	static void swap(Ep a, Ep b) {
+		Ep c = a;
+		a = b;
+		b = c;
+		System.out.println(b.x);
+	}
+}
+
+class Ep {
+	Ep(int y) {
+		x = y;
+	}
+	public int x;
 }
